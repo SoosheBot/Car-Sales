5e964422a77d354bd7c54c00b23c46c2a747035d
sooshe <sooshe@sooshebot.com>
2020-01-22 16:32:06 -0600

    update state to props in App, add mapStateToProps, debug AdditionalFeatures

--- src/App.js
+++ src/App.js
@@ -5,29 +5,40 @@ import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
-const App = () => {
+import { connect } from 'react-redux';
+
+
+const App = (props) => {
   
 
-  const removeFeature = item => {
-    // dispatch an action here to remove an item
-  };
+  // const removeFeature = item => {
+  //   // dispatch an action here to remove an item
+  // };
 
-  const buyItem = item => {
-    // dipsatch an action here to add an item
-  };
+  // const buyItem = item => {
+  //   // dipsatch an action here to add an item
+  // };
 
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header car={props.carOnProps} />
+        <AddedFeatures car={props.carOnProps} />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures additionalFeatures={props.additionalFeatures} />
+        <Total car={props.carOnProps} additionalPrice={props.additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    carOnProps: state.car,
+    store: state.store,
+    additionalPrice: state.additionalPrice
+  };
+};
+
+export default connect(mapStateToProps, {})(App); 
--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AdditionalFeature = props => {
+  const [newFeature, setNewFeature] = useState();
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button" onClick={()=> props.addFeature(state.newFeature)}>Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
