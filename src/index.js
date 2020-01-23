69e0cb1fe77c837195aa3748e6f49bca10f05e8a
sooshe <sooshe@sooshebot.com>
2020-01-22 19:25:56 -0600

    create notes app and save current work in ROOT index.js file

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+//Step 1 -- import Provider and createStore, add to rootElement
+// Step 2 -- wrap App in Provider
+// Step 3 -- create a store function to pass into Provider
+
+const store = createStore();
+console.log('store state is', store.getState());
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
