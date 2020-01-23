721843f48dccf379f84d95fca1dd052b93e1df34
sooshe <sooshe@sooshebot.com>
2020-01-22 23:42:01 -0600

    add onClick feature to button in AddedFeature file

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import {removeFeature } from '../actions/addedFeaturesActions';
 
 const AddedFeature = props => {
+  // const [addedFeature, setAddedFeature] = useState();
+
+  // const handleChanges = e => {
+  //   setAddedFeature({ addedFeature: e.target.value})
+  // };
+
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button" onClick={() => props.removeFeature={removeFeature}}>X</button>
       {props.feature.name}
     </li>
   );
