b6b1b8d3938ffac26a444ce0e2f98eabc49f705b
sooshe <sooshe@sooshebot.com>
2020-01-24 15:34:09 -0600

    mvp

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
-import { removeFeature } from '../actions/addedFeaturesActions';
 
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button" onClick={() => props.removeFeature={removeFeature}}>X</button>
+      <button className="button" onClick={() => {
+        props.removeFeature(props.feature)
+      }}>X</button>
       {props.feature.name}
     </li>
   );
 };
 
 export default AddedFeature;
+
--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -1,47 +1,21 @@
-import React, { useState } from 'react';
-
+import React from 'react';
 import AddedFeature from './AddedFeature';
 
-import { connect } from 'react-redux';
-import { addFeature, removeFeature } from '../actions/addedFeaturesActions';
-
-
 const AddedFeatures = props => {
-  const [addedFeatures, setAddedFeatures] = useState();
-
-  console.log('props', props);
-
-  // const handleChanges = e => {
-  //   setAddedFeatures(e.target.value);
-  // };
-
   return (
     <div className="content">
       <h6>Added features:</h6>
       {props.car.features.length ? (
         <ol type="1">
           {props.car.features.map(item => (
-            <AddedFeature key={item.id} feature={item} removeFeature={removeFeature}/>
+            <AddedFeature key={item.id} feature={item} removeFeature={props.removeFeature} />
           ))}
         </ol>
       ) : (
-        <div>
         <p>You can purchase items from the store.</p>
-        <button onClick={() => props.addFeature(addedFeatures)}>Add Feature</button>
-        </div>
       )}
     </div>
   );
 };
 
-
-const mapStateToProps = state => {
-  return {
-      car: state.addedFeaturesReducer
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  { addFeature }
-)(AddedFeatures);
+export default AddedFeatures;
