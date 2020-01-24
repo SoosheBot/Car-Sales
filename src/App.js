adb5cbb40dfd546d29cdfa9d7ed3d9ab7810fd0e
sooshe <sooshe@sooshebot.com>
2020-01-24 15:31:50 -0600

    update src app file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React from 'react';
 
-// import Header from "./components/Header";
-import AddedFeatures from "./components/AddedFeatures";
-// import AdditionalFeatures from "./components/AdditionalFeatures";
-// import Total from "./components/Total";
+import Header from './components/Header';
+import AddedFeatures from './components/AddedFeatures';
+import AdditionalFeatures from './components/AdditionalFeatures';
+import Total from './components/Total';
+import { connect } from 'react-redux';
+import { addFeature, removeFeature } from './actions';
 
-const App = props => {
+const App = (props) => {
+
+  const removeFeature = feature => {
+    props.removeFeature(feature);
+  }
+
+  const addFeature = item => {
+    props.addFeature(item);
+  }
 
 
   return (
     <div className="boxes">
       <div className="box">
-        {/* <Header car={props.car} /> */}
-        {/* <Header /> */}
-        <AddedFeatures />
+        <Header car={props.car} />
+        <AddedFeatures car={props.car} removeFeature={removeFeature}/>
       </div>
       <div className="box">
-        {/* <AdditionalFeatures additionalFeatures={props.additionalFeatures} /> */}
-        {/* <Total car={props.car} additionalPrice={props.additionalPrice} /> */}
+        <AdditionalFeatures store={props.store} addFeature={addFeature}/>
+        <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+const mapStateToProps = state => {
+  return{
+    car:state.car,
+    store:state.store,
+    additionalProce:state.additionalPrice
+  }
+}
+
+export default connect(mapStateToProps, {addFeature, removeFeature})(App);
